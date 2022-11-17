7acb2fcb80676c0bba8e4b6ecdaef5a0fc574eb4
Bhavani <lgbhavani428@gmail.com>
2022-11-17 21:17:48 +0530

    Update vacation_route.js

--- vacation_route.js
+++ vacation_route.js
@@ -1,31 +1,3 @@
-
-// const startPoint = "Kiev";
-// const source = ["Paris", "Zurich", "Prague", "Barcelona", "Kiev", "Skopje", "Amsterdam", "Berlin", "Berlin"];
-// const destination = ["Skopje", "Amsterdam", "Zurich", "Berlin", "Prague", "Paris", "Barcelona", "Kiev", "Amsterdam"];
-
-// // source.map((index) => {
-// //     console.log("Source", source);
-// // })
-
-// let idx = 0;
-// let nextSource = startPoint;
-// for (idx = 0; idx < source.length; idx++) {
-//     if (nextSource === source[idx]) {
-//         nextSource = destination[idx];
-//         console.log(source[idx] +"-"+ destination[idx]);
-//         idx = 0;
-//         if (nextSource === startPoint) {
-//            return
-//         }
-//     }
-// }
-
-
-
-
-
-
-
 let visitedCities = ['Amsterdam', 'Kiev', 'Zurich', 'Prague', 'Barlin', 'Barcelona'];
 let tickets = [ 'Kiev-Prague', 'Prague-Zurich', 'Zurich-Amsterdam', 'Amsterdam-Barcelona', 'Barcelona-Barlin', 'Barlin-Kiev'];
 
